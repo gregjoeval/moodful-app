6d8afc0dd3c300490f9d844f54da74292b2f981b
Greg Valentine <gregjoeval@gmail.com>
2021-05-02 00:02:44 -0400

    Update WithJWT.tsx

--- src/higher-order-components/WithJWT.tsx
+++ src/higher-order-components/WithJWT.tsx
@@ -1,5 +1,5 @@
 import { useAuth0 } from '@auth0/auth0-react'
-import { DateTime } from 'luxon'
+import { DateTime, Duration } from 'luxon'
 import React, { useCallback, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { ConfigurationDuck } from '../features/configuration'
@@ -7,10 +7,20 @@ import usePolling from '../hooks/UsePolling'
 
 const accessTokenStorageKey = 'AccessToken'
 
-const parseToken = (token: string): Record<string, string> => {
+interface IJsonWebTokenPayload {
+    iss: string,
+    sub: string,
+    aud: Array<string>,
+    iat: number,
+    exp: number,
+    azp: string,
+    scope: string
+}
+
+const parseToken = (token: string): IJsonWebTokenPayload => {
     const payload = token.split(/[.]/)[1]
     const decodedToken = window.atob(payload)
-    return JSON.parse(decodedToken) as Record<string, string>
+    return JSON.parse(decodedToken) as IJsonWebTokenPayload
 }
 const getToken = (): string | null => window.localStorage.getItem(accessTokenStorageKey)
 const setToken = (value: string): void => window.localStorage.setItem(accessTokenStorageKey, value)
@@ -35,19 +45,18 @@ export const getSubjectFromAccessToken = (): string | null => {
     return sub
 }
 
-const intervalWithToken = 15 * 60 * 1000
-const intervalWithoutToken = 3 * 1000
+const intervalWithToken = Duration.fromObject({ minutes: 15 }).toMillis()
+const intervalWithoutToken = Duration.fromObject({ seconds: 3 }).toMillis()
 
 const withJWT = <Props extends Record<string, unknown>> (Component: React.ComponentType<Props>): React.FunctionComponent<Props> => (props) => {
     const [isLoading, setIsLoading] = useState(false)
-
     const token = getToken()
-
     const { getAccessTokenSilently, isAuthenticated } = useAuth0()
     const configurationModel = useSelector(ConfigurationDuck.Selectors.selectModel)
 
     const getAccessTokenAsync = useCallback(async () => {
         setIsLoading(true)
+        removeToken()
         try {
             const accessToken = await getAccessTokenSilently({
                 audience: configurationModel.MoodfulApiUri,
@@ -67,18 +76,12 @@ const withJWT = <Props extends Record<string, unknown>> (Component: React.Compon
             if (!isLoading) {
                 // eslint-disable-next-line no-console
                 console.debug('Checking access token...')
-
-                if (!isAuthenticated) {
-                    removeToken()
-                } else if (token === null) {
+                if (!isAuthenticated || token === null) {
                     void getAccessTokenAsync()
                 } else {
                     const { exp } = parseToken(token)
-                    const expiration = Number.parseInt(exp, 10)
-                    const differenceInSeconds = DateTime.fromMillis(expiration)
-                        .diffNow('seconds')
-                        .as('seconds')
-                    if (differenceInSeconds >= 0) {
+                    const differenceInSeconds = DateTime.fromMillis(exp).diffNow('seconds').as('seconds')
+                    if (differenceInSeconds <= 0) {
                         // eslint-disable-next-line no-console
                         console.debug('Access token has expired.')
                         void getAccessTokenAsync()
